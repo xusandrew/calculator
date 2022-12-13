84962318283bd6943d0b14b7baa22d9521063853
Andrew Xu <xusandrew@gmail.com>
2022-12-12 20:34:27 -0500

    Select HTML elements

---
+++ index.js
@@ -0,0 +1,17 @@
+const display = document.querySelector(".display");
+const num1 = document.querySelector(".1");
+const num2 = document.querySelector(".2");
+const num3 = document.querySelector(".3");
+const num4 = document.querySelector(".4");
+const num5 = document.querySelector(".5");
+const num6 = document.querySelector(".6");
+const num7 = document.querySelector(".7");
+const num8 = document.querySelector(".8");
+const num9 = document.querySelector(".9");
+const divideButton = document.querySelector(".divide");
+const multiplyButton = document.querySelector(".multiply");
+const subtractButton = document.querySelector(".subtract");
+const addButton = document.querySelector(".add");
+const equalsButton = document.querySelector(".equals");
+const button = document.querySelector(".decimal");
+
