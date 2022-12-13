4c258204b5b7e4ac9adfd92f9519331f77f935e2
Andrew Xu <xusandrew@gmail.com>
2022-12-12 20:49:28 -0500

    Add event listeners to numButtons

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const display = document.querySelector(".display");
 const numButtons = [
-  document.querySelector(".1"),
-  document.querySelector(".2"),
-  document.querySelector(".3"),
-  document.querySelector(".4"),
-  document.querySelector(".5"),
-  document.querySelector(".6"),
-  document.querySelector(".7"),
-  document.querySelector(".8"),
-  document.querySelector(".9"),
+  document.querySelector(".num1"),
+  document.querySelector(".num2"),
+  document.querySelector(".num3"),
+  document.querySelector(".num4"),
+  document.querySelector(".num5"),
+  document.querySelector(".num6"),
+  document.querySelector(".num7"),
+  document.querySelector(".num8"),
+  document.querySelector(".num9"),
 ];
+
+
 const divideButton = document.querySelector(".divide");
 const multiplyButton = document.querySelector(".multiply");
 const subtractButton = document.querySelector(".subtract");
 const addButton = document.querySelector(".add");
 const equalsButton = document.querySelector(".equals");
 const button = document.querySelector(".clear");
+
+var result = 0;
+var operation = NaN;
+
+for (let i = 0; i < 9;i++){
+    numButtons[i].addEventListener('click', () => {
+        if (result === 0) {
+            result = i + 1;
+            return;
+        }
+        result += (i+1).toString();
+    });
+}
