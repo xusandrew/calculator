c4b3d2727d558fa15c5967c66037d46b372b65dd
Andrew Xu <xusandrew@gmail.com>
2022-12-12 20:42:36 -0500

    Make numButtons array

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const display = document.querySelector(".display");
-const num1 = document.querySelector(".1");
-const num2 = document.querySelector(".2");
-const num3 = document.querySelector(".3");
-const num4 = document.querySelector(".4");
-const num5 = document.querySelector(".5");
-const num6 = document.querySelector(".6");
-const num7 = document.querySelector(".7");
-const num8 = document.querySelector(".8");
-const num9 = document.querySelector(".9");
+const numButtons = [
+  document.querySelector(".1"),
+  document.querySelector(".2"),
+  document.querySelector(".3"),
+  document.querySelector(".4"),
+  document.querySelector(".5"),
+  document.querySelector(".6"),
+  document.querySelector(".7"),
+  document.querySelector(".8"),
+  document.querySelector(".9"),
+];
 const divideButton = document.querySelector(".divide");
 const multiplyButton = document.querySelector(".multiply");
 const subtractButton = document.querySelector(".subtract");
